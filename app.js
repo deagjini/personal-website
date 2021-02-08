bc03198cf4c2909ef5e2e5c5805bb92e1d133b83
deagjini <dea@easicloud.ca>
2021-02-08 18:21:10 -0500

    added color-changing option to change the accent colors of the site via button

--- app.js
+++ app.js
@@ -21,4 +21,43 @@ function navSlide() {
     
 }
 
+function changeColor(){
+    var paragraph = document.getElementById("color-description");
+    var link = document.getElementById("color-link"); 
+    var spanColor1 = document.getElementById("color1");
+    var spanColor2 = document.getElementById("color2");  
+
+    var changes = new Array(paragraph, spanColor1, spanColor2); 
+
+    document.querySelector(".change-color").addEventListener('click', () =>{
+        document.body.style.transitionDelay = "2s"; 
+        document.body.classList.toggle('dark');
+
+        //change pantone link & the titles of the spans 
+        if (document.body.classList == 'dark'){
+            link.setAttribute('href', 'https://www.pantone.com/color-of-the-year-2021');
+            spanColor1.title = '#F5DF4D';
+            spanColor2.title = '#939597'; 
+        } else {
+            link.setAttribute('href', 'https://www.pantone.com/articles/color-of-the-year/color-of-the-year-2016');
+            spanColor1.title = '#F7CACA'; 
+            spanColor2.title = '#B3CEE5'; 
+        }
+        
+        //change text in color title & description 
+        for (i = 0; i < changes.length; i++){
+            ob = changes[i]; 
+            if (ob.getAttribute("data-text-swap") == ob.innerHTML) {
+                ob.innerHTML = ob.getAttribute("data-text-original");
+            } else {
+                ob.setAttribute("data-text-original", ob.innerHTML);
+                ob.innerHTML = ob.getAttribute("data-text-swap");
+            }
+        }
+
+
+    })
+}
+
 navSlide();
+changeColor();
